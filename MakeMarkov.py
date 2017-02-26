1b870e073a4f6f86aed357d164bb7141f8bb2acb
jeimer398 <josheimer98@gmail.com>
2017-02-26 12:50:59 -0500

    stuff

--- MakeMarkov.py
+++ MakeMarkov.py
@@ -4,10 +4,8 @@ import markovify
 import sys
 
 def makeMarkov(twitter_handle):
-    text = grabber(twitter_handle)
     print(text)
     text_model = markovify.NewlineText(text)
-    print("TEST TEST TEST TEST TEST TEST TEST")
     for i in range(5):
         print(text_model.make_sentence())
     for i in range(3):
