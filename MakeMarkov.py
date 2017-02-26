2ec620e683b8545a656c9ade71f27d9617812dd0
jeimer398 <josheimer98@gmail.com>
2017-02-26 11:08:40 -0500

    Create MakeMarkov.py

---
+++ MakeMarkov.py
@@ -0,0 +1,11 @@
+import twitter
+from Grabber import *
+import markovify
+
+def makeMarkov(twitter_handle):
+    text = grabber(twitter_handle)
+    text_model = markovify.Text(text)
+    for i in range(5):
+        print(text_model.make_sentence())
+    for i in range(3):
+        print(text_model.make_short_sentence(140))
