2278f41435fb819b34feaa18a3a1699796856b7d
jeimer398 <josheimer98@gmail.com>
2017-02-25 21:40:31 -0500

    buttons to navigate webpage

--- Markov.js
+++ Markov.js
@@ -1,1 +1,10 @@
-alert("hello world");
+function display() {
+  var twitterHandle = prompt("Enter a Twitter Handle");
+  if(twitterHandle != null) {
+    document.getElementById("demo").innerHTML = "Searching for " + twitterHandle;
+    document.getElementById("startbttn").style.display = 'none';
+    document.getElementById("ready").style.display = 'block';
+  } else {
+    document.getElementById("demo").innerHTML = "oh ok";
+  }
+}
