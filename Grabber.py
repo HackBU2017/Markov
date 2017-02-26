c6dbc74c96053109b48b1062d1c26d4dc0ea4ea4
Dino Saur <mwilomovsky83@gmail.com>
2017-02-26 11:43:57 -0500

    (Super Grabber)

--- Grabber.py
+++ Grabber.py
@@ -1,19 +1,31 @@
+#!/usr/bin/env python
+# encoding: utf-8
 import twitter
 import markovify
+import tweepy
 
-def grabber(twitter_handle):
-    api = twitter.Api(consumer_key='BQRfMJOhdiARaRBwlOpo78ktp',
-      consumer_secret='RxF8PngRe44jVMQDUdunfTy9KLRCqbp9NKFYX3fuHFkx2MvsHX',
-      access_token_key='2498510321-AC9ycNnMzoug1iniU544RvceQkRNQxOS25qdaTr',
-      access_token_secret='5odRjLJdgntK0mffliAbIARphkqDyPrJ0UjqeUMI9bmo7')
+def grabber():
+	consumer_key = "BQRfMJOhdiARaRBwlOpo78ktp"
+	consumer_secret="RxF8PngRe44jVMQDUdunfTy9KLRCqbp9NKFYX3fuHFkx2MvsHX"
+	access_key="2498510321-AC9ycNnMzoug1iniU544RvceQkRNQxOS25qdaTr"
+	access_secret="5odRjLJdgntK0mffliAbIARphkqDyPrJ0UjqeUMI9bmo7"
 
-    {"id": 16133, "location": "Philadelphia", "name": "bear"}
-
-    t = api.GetUserTimeline(screen_name='RealDonaldTrump')
-
-    longString = ""
-
-    tweets = [i.AsDict() for i in t]
-    for t in tweets:
-        longString += str(t['text'])
-    return longString
+	screen_name='realdonaldtrump'
+	auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+	auth.set_access_token(access_key, access_secret)
+	api = tweepy.API(auth)
+	alltweets = []	
+	
+	new_tweets = api.user_timeline(screen_name = screen_name,count=200)
+	alltweets.extend(new_tweets)
+	oldest = alltweets[-1].id - 1
+	
+	while len(new_tweets) > 0:
+		print ("getting tweets before %s" % (oldest))
+		new_tweets = api.user_timeline(screen_name = screen_name,count=200,max_id=oldest)
+		alltweets.extend(new_tweets)
+		oldest = alltweets[-1].id - 1	
+		print ("...%s tweets downloaded so far" % (len(alltweets)))
+	outtweets = [[tweet.text.encode("utf-8")] for tweet in alltweets]
+	print (outtweets)
+	return outtweets
