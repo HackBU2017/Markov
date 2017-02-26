5ae4072062e5d525afade12375154570a14792c7
jeimer398 <josheimer98@gmail.com>
2017-02-26 12:52:01 -0500

    Update Grabber.py

--- Grabber.py
+++ Grabber.py
@@ -1,43 +1,41 @@
-#!/usr/bin/env python
-# encoding: utf-8
 import twitter
 import markovify
-import tweepy
 
-def grabber():
-	consumer_key = "BQRfMJOhdiARaRBwlOpo78ktp"
-	consumer_secret="RxF8PngRe44jVMQDUdunfTy9KLRCqbp9NKFYX3fuHFkx2MvsHX"
-	access_key="2498510321-AC9ycNnMzoug1iniU544RvceQkRNQxOS25qdaTr"
-	access_secret="5odRjLJdgntK0mffliAbIARphkqDyPrJ0UjqeUMI9bmo7"
+def grabber(twitter_handle):
+    api = twitter.Api(consumer_key='BQRfMJOhdiARaRBwlOpo78ktp',
+    consumer_secret='RxF8PngRe44jVMQDUdunfTy9KLRCqbp9NKFYX3fuHFkx2MvsHX',
+    access_token_key='2498510321-AC9ycNnMzoug1iniU544RvceQkRNQxOS25qdaTr',
+    access_token_secret='5odRjLJdgntK0mffliAbIARphkqDyPrJ0UjqeUMI9bmo7')
 
-<<<<<<< HEAD
     t = api.GetUserTimeline(screen_name=twitter_handle, count = 200)
 
     longString = ""
 
     tweets = [i.AsDict() for i in t]
-    # return [str(t['text']).encode("utf-8") for t in tweets]
+    #longString += [str(t['text'].encode("utf-8") for t in tweets]
+    #return longString
     for t in tweets:
+    #     longString += str(t['text'])
+         longString += str(t['text'].encode("utf-8") for t in tweets)
+         return longString
         longString += str(t['text'].encode("utf-8"))
     return longString
-=======
 	screen_name='realdonaldtrump'
 	auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 	auth.set_access_token(access_key, access_secret)
 	api = tweepy.API(auth)
-	alltweets = []	
-	
+	alltweets = []
+
 	new_tweets = api.user_timeline(screen_name = screen_name,count=200)
 	alltweets.extend(new_tweets)
 	oldest = alltweets[-1].id - 1
-	
+
 	while len(new_tweets) > 0:
 		print ("getting tweets before %s" % (oldest))
 		new_tweets = api.user_timeline(screen_name = screen_name,count=200,max_id=oldest)
 		alltweets.extend(new_tweets)
-		oldest = alltweets[-1].id - 1	
+		oldest = alltweets[-1].id - 1
 		print ("...%s tweets downloaded so far" % (len(alltweets)))
 	outtweets = [[tweet.text.encode("utf-8")] for tweet in alltweets]
 	print (outtweets)
 	return outtweets
->>>>>>> 19e07a6ea10638c16507242ac8799f28cf2163db
