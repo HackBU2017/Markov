ea682a2c4df2ae29e0c24ba99d146cd6f05d4e68
jeimer398 <josheimer98@gmail.com>
2017-02-26 11:07:01 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,13 +2,17 @@ from flask import Flask, render_template, request
 import sys
 import twitter
 from Grabber import *
+from MakeMarkov import *
 
 app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
 def home():
-    # return makeMarkov(grabber())
-    return render_template('Markov.html')
+    if request.method == 'POST':
+        tweet = makeMarkov(request.form['handle'])
+        return render_template('Markov.html', tweet=tweet, itried=True)
+    else:
+        return render_template('Markov.html')
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
