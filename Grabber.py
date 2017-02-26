43f3aeeb760531d697f829e1cef1b7136f464d19
Matt Wilomovsky <mwilomovsky83@gmail.com>
2017-02-26 12:15:31 -0500

    Update Grabber.py

--- Grabber.py
+++ Grabber.py
@@ -31,7 +31,7 @@ def grabber():
 	alltweets.extend(new_tweets)
 	oldest = alltweets[-1].id - 1
 	
-	while len(alltweets) < 2000:
+	while len(new_tweets) > 0:
 		print ("getting tweets before %s" % (oldest))
 		new_tweets = api.user_timeline(screen_name = screen_name,count=200,max_id=oldest)
 		alltweets.extend(new_tweets)
